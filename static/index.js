a1420057d1713f1c3100ff4856cfb915c2c65287
gc <1790758006@qq.com>
2022-01-24 19:46:10 +0800

    文字

--- static/index.js
+++ static/index.js
@@ -337,14 +337,15 @@ function backBtn() {
 function shareText(score) {
     var date2 = new Date();
     deviation_time = (date2.getTime() - _date1.getTime())
+
     if (deviation_time > 23000) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '试着好好练一下？';
-    if (score <= 99) return 'TCL';
-    if (score <= 149) return 'TQL';
-    if (score <= 199) return '您';
+    if (score <= 49) return '金轮不理你了';
+    if (score <= 99) return '金轮对你说加油';
+    if (score <= 149) return '这是金轮的皮燕子？';
+    if (score <= 199) return '哈哈哈哈哈哈哈';
     return '人？';
 }
 
